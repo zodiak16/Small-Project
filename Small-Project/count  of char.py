1ec8ed7d8d1a3428ecaf9604682446120d25e640
Amirhossein.Shahabi <99580514+zodiak16@users.noreply.github.com>
2024-02-11 09:18:33 +0330

    Update count  of char.py

--- Small-Project/count  of char.py
+++ Small-Project/count  of char.py
@@ -5,6 +5,6 @@
 # برنامھای بنویسید کھ رشتھای و حرفی را از کاربر دریافت کند و تعداد تکرار آن حرف در رشتھ را چاپ کند
 # .برای مثال با دریافت” banana “و a ، “3”را چاپ کند 
 
-w=input("Enter world: ")
+w=input("Enter word: ")
 z=input("Enter character: ")
 print(w.count(z))
